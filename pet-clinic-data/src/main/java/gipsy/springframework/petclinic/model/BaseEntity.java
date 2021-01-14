39b627784ed66578ad0571346dfeeec8f53356b7
crobbere <53408506+crobbere@users.noreply.github.com>
2021-01-14 22:04:08 +0100

    adding base entity class + extends

---
+++ pet-clinic-data/src/main/java/gipsy/springframework/petclinic/model/BaseEntity.java
@@ -0,0 +1,19 @@
+package gipsy.springframework.petclinic.model;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
