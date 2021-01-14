d6e676f00ca4100ad20a7ab65392f5025e37b0ec
crobbere <53408506+crobbere@users.noreply.github.com>
2021-01-14 20:21:58 +0100

    adding plain old java objects

---
+++ src/main/java/gipsy/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package gipsy.springframework.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/gipsy/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package gipsy.springframework.petclinic.model;
+
+public class Vet extends Person{
+}
