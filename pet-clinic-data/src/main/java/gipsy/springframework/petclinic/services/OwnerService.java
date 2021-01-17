e250f30105697cca86e763395526cf4912d0c1bf
crobbere <53408506+crobbere@users.noreply.github.com>
2021-01-17 18:13:18 +0100

    centralizing services to one interface

--- pet-clinic-data/src/main/java/gipsy/springframework/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/gipsy/springframework/petclinic/services/OwnerService.java
@@ -1,18 +1,10 @@
 package gipsy.springframework.petclinic.services;
 
 import gipsy.springframework.petclinic.model.Owner;
-import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
-@Service
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
 
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
