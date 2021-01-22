c18b3c1f040385328c5b4aacdcab01f2b1d645ee
crobbere <53408506+crobbere@users.noreply.github.com>
2021-01-22 18:09:16 +0100

    Create SpecialityService and mapservice closes #15

---
+++ pet-clinic-data/src/main/java/gipsy/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package gipsy.springframework.petclinic.services;
+
+import gipsy.springframework.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
