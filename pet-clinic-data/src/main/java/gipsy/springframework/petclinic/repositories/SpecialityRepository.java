63112f4be1d0c25a41a6b64942507f36786c7e9c
crobbere <53408506+crobbere@users.noreply.github.com>
2021-01-25 17:01:58 +0100

    added spring data JPA repos closes #23

---
+++ pet-clinic-data/src/main/java/gipsy/springframework/petclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,7 @@
+package gipsy.springframework.petclinic.repositories;
+
+import gipsy.springframework.petclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+}
