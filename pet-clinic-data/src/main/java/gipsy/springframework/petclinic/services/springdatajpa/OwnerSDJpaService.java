e7337c8244be6b7b6a1956fd076baa78d152cdf0
crobbere <53408506+crobbere@users.noreply.github.com>
2021-02-01 18:52:06 +0100

    Created CRUD Tests for Owner JPA Service closes #34

--- pet-clinic-data/src/main/java/gipsy/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/gipsy/springframework/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -50,12 +50,7 @@ public class OwnerSDJpaService implements OwnerService {
 
     @Override
     public Owner save(Owner object) {
-        System.out.println("###########################");
-        System.out.println("##########################");
-        System.out.println("#########################");
-        System.out.println("########################");
-        System.out.println("#######################");
-        System.out.println("OwnerSDJpaService working.");
+
         return ownerRepository.save(object);
     }
 
